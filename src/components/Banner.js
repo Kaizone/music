d7de58294635d7532fed09b59557806d0493cc32
Kaizone <949927499@qq.com>
2018-04-15 10:56:29 +0800

    new

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@
 import React,{Component} from 'react';
 
 import axios from 'axios';
-
+import './tween.js';
 class Banner extends React.Component{
 
     constructor(args){
@@ -30,12 +30,24 @@ class Banner extends React.Component{
 
     }
 
+    toLeft(){
+        console.log(this.refs.children);
+    }
 
     render(){
         console.log(this.state.datas);
         return(
             <div>
-                <ul className="banner-list clear">
+                <ul  ref="lis" className="banner-list clear">
+                    <li className="side">
+                        <img src="http://p3.music.126.net/s25q2x5QyqsAzilCurD-2w==/7973658325212564.jpg"/>
+                    </li>
+                    <li className="middle">
+                        <img src="http://p4.music.126.net/tGPljf-IMOCyPvumoWLOTg==/7987951976374270.jpg"/>
+                    </li>
+                    <li className="side">
+                        <img src="http://p3.music.126.net/e0gGadEhjur2UuUpDF9hPg==/7788940372125389.jpg"/>
+                    </li>
                     <li className="side">
                         <img src="http://p3.music.126.net/s25q2x5QyqsAzilCurD-2w==/7973658325212564.jpg"/>
                     </li>
@@ -54,7 +66,7 @@ class Banner extends React.Component{
                     <span></span>
                     <span></span>
                 </p>
-                <span className="prev"> &lt; </span>
+                <span className="prev" onClick={this.toLeft.bind(this)}> &lt; </span>
                 <span className="next"> &gt; </span>
             </div>
         )
